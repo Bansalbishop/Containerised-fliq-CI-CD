01a757fc214c93e0630d4d6e813f3001a52bb230
bb <bishopbansal2005@gmail.com>
2026-03-13 00:12:29 +0530

    fix: update axios baseURL to include API path for backend URL

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_BACKEND_URL || "http://localhost:5001",
+  baseURL:
+    import.meta.env.VITE_BACKEND_URL + "/api" || "http://localhost:5001/api",
   withCredentials: true,
 });
