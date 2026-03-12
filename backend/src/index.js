d5b9ab2e5a750b27951c92fc314df2c5efbfc770
bb <bishopbansal2005@gmail.com>
2026-03-12 23:23:40 +0530

    feat: add .gitignore, update package.json scripts, and configure production settings for static files

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,10 +5,12 @@ import dotenv from "dotenv";
 import cors from "cors";
 
 import cookieParser from "cookie-parser";
+import path from "path";
 import { connectDB } from "./lib/db.js";
 import { app, server } from "./lib/socket.js";
 dotenv.config();
 const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ limit: "10mb", extended: true }));
 app.use(cookieParser());
@@ -20,6 +22,13 @@ app.use(
 );
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 server.listen(PORT, () => {
   console.log("server is running on port PORT " + PORT);
