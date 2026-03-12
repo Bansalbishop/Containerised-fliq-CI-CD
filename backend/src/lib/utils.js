db6a89f2aca0c86d0f359ff1cadd04ca58e2cd92
bb <bishopbansal2005@gmail.com>
2026-03-13 00:18:02 +0530

    fix: update cookie settings to allow cross-site cookies and enhance security

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -1,15 +1,19 @@
 import jwt from "jsonwebtoken";
 import env from "dotenv";
+
 env.config();
+
 export const generatetoken = (userId, res) => {
   const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
     expiresIn: "3d",
   });
+
   res.cookie("jwt", token, {
     maxAge: 3 * 24 * 60 * 60 * 1000,
-    httpOnly: true, //prevents xss attacks that is cross site scripting attacks
-    sameSite: "strict", //CSRF attack cross site request forgery attacks
-    secure: process.env.NODE_ENV !== "development",
+    httpOnly: true,
+    sameSite: "none", // allow cross-site cookies (Vercel → Render)
+    secure: true, // required when sameSite is "none"
   });
+
   return token;
 };
