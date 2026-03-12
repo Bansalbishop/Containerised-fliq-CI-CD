a3a48a6269b6d4b6555756bc5b7f632573d56773
bb <bishopbansal2005@gmail.com>
2026-03-12 23:49:54 +0530

    fix: remove catch-all route for serving index.html in production

--- backend/src/index.js
+++ backend/src/index.js
@@ -24,10 +24,6 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
-
-  app.get("/:path(*)", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
-  });
 }
 
 server.listen(PORT, () => {
